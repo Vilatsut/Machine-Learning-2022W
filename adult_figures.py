7abea3926ce2737c6b000d24f50eb64fa8f4323e
Ville Hokkinen <e12202307@student.tuwien.ac.at>
2022-11-13 14:13:22 +0100

    Cleaning up the code

--- adult_figures.py
+++ adult_figures.py
@@ -1,17 +1,18 @@
 # %%
 import pandas as pd
+import os
 # %%
-df_adults = pd.read_csv(r'C:\Users\beregszaszim\Desktop\TU_Wien\ML\Exercise_0\adults\adults-clean.csv')
+df_adults = pd.read_csv(os.getcwd() + '/data/clean_adults.csv')
 # %% education_num chart
-df_adults_figure1 = df_adults[['education-num','class']]
-df_adults_figure1_dummies = pd.get_dummies(df_adults_figure1, columns=['class'])
+df_adults_figure1 = df_adults[['education.num','income']]
+df_adults_figure1_dummies = pd.get_dummies(df_adults_figure1, columns=['income'])
 # %%
-df_adults_figure1_grouped = df_adults_figure1_dummies.groupby(by='education-num').sum()
+df_adults_figure1_grouped = df_adults_figure1_dummies.groupby(by='education.num').sum()
 df_adults_figure1_grouped.rename(columns={'class_ <=50K':'<=50K', 'class_ >50K':'50K'}, inplace = True)
 # %%
 import matplotlib.pyplot as plt
 
 df_adults_figure1_grouped.plot(kind='bar', stacked=True)
-plt.savefig(r'C:\Users\beregszaszim\Desktop\TU_Wien\ML\Exercise_0\adults\adults_figure.png', dpi=200)
+plt.savefig(r'./figures/adults_figure.png', dpi=200)
 plt.show()
 # %%
