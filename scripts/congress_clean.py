444e61ff007292c2d51e22005f45200b419e7c0b
Ville Hokkinen <e12202307@student.tuwien.ac.at>
2022-11-14 18:29:33 +0100

    Clean dumb code

--- scripts/congress_clean.py
+++ scripts/congress_clean.py
@@ -15,11 +15,9 @@ label_encoder = preprocessing.LabelEncoder()
 columns = ["handicapped-infants","water-project-cost-sharing","adoption-of-the-budget-resolution","physician-fee-freeze","el-salvador-aid","religious-groups-in-schools","anti-satellite-test-ban","aid-to-nicaraguan-contras","mx-missile","immigration","synfuels-crporation-cutback","education-spending","superfund-right-to-sue","crime","duty-free-exports","export-administration-act-south-africa"]
 for column in columns:
     df[column] = label_encoder.fit_transform(df[column])
-
-columns = ["handicapped-infants","water-project-cost-sharing","adoption-of-the-budget-resolution","physician-fee-freeze","el-salvador-aid","religious-groups-in-schools","anti-satellite-test-ban","aid-to-nicaraguan-contras","mx-missile","immigration","synfuels-crporation-cutback","education-spending","superfund-right-to-sue","crime","duty-free-exports","export-administration-act-south-africa"]
-for column in columns:
     test_df[column] = label_encoder.fit_transform(test_df[column])
 
+# Save to files
 df.to_csv("data/congress/clean_congress.csv")
 test_df.to_csv("data/congress/clean_test_congress.csv")
 sol_df.to_csv("data/congress/clean_sol_congress.csv")
