acd6fce7e5bd93069e95b8e996465d4081800e01
Ville Hokkinen <e12202307@student.tuwien.ac.at>
2022-11-20 14:42:41 +0100

    Add kaggle competition files

--- scripts/congress_model.py
+++ scripts/congress_model.py
@@ -22,6 +22,23 @@ y_test = test_df["class"]
 # DECISION TREE CLASSIFIER
 experiment_DT_classifier(X_train, y_train, X_test,y_test)
 
+
+# Best classifier based on f1-score running the Kaggle competition data
+
+# from sklearn.tree import DecisionTreeClassifier
+# competition_df = pd.read_csv(os.getcwd() + "/data/congress/clean_comp_congress.csv", index_col=0)
+# dt = DecisionTreeClassifier(criterion="gini", splitter="best", max_depth=10)
+# # Training
+# dt.fit(X_train, y_train)
+# # Predicting
+# dt_pred = dt.predict(competition_df)
+# f = open('competition.csv', 'w')
+# new_df = pd.DataFrame(data=dt_pred, index=competition_df.index, columns=["class"])
+# new_df["class"] = new_df["class"].replace(0, "democrat")
+# new_df["class"] = new_df["class"].replace(1, "republican")
+# new_df.to_csv("data/congress/comp_result.csv")
+
+
 # KNN CLASSIFIER
 experiment_KN_classifier(X_train, y_train, X_test,y_test)
 
