4c5f4543299b7ca52e4f8c810ab48d0320c21726
Ville Hokkinen <e12202307@student.tuwien.ac.at>
2022-12-14 15:54:14 +0100

    Bayes start

--- scripts/naive_bayes.py
+++ scripts/naive_bayes.py
@@ -1,7 +1,23 @@
 import pandas as pd
 import os
+import torch.nn as nn
 
 
+class NaiveBayesClassifier():
+  def __init__(self):
+    ###################################
+    # Write your own code here #
+    pass
+
+    ###################################
+
+  def forward(self, x):
+    ###################################
+    # Write your own code here #
+
+
+    ###################################
+    return x
 
 
 df = pd.read_csv(os.getcwd() + "/data/congress/clean_train_congress.csv", index_col=0)
@@ -12,3 +28,8 @@ X_train = df[df.columns[:-1]]
 y_train = df["class"]
 X_test = test_df[test_df.columns[:-1]]
 y_test = test_df["class"]
+
+skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=None)
+
+for train_index, test_index in skf.split(X,y_binned):
+    pass
