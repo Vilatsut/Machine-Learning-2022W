87557ec4fa0eefff22dd94c580c47027ae3f56a6
Tim Pangresic <tim.pangersic@gmail.com>
2023-01-11 23:17:52 +0100

    added comments to naive bayes alg

--- EX2/scripts/own_naive_bayes.py
+++ EX2/scripts/own_naive_bayes.py
@@ -6,26 +6,26 @@ from sklearn.base import ClassifierMixin, BaseEstimator
 
 class OwnNaiveBayes(BaseEstimator, ClassifierMixin):
     def __init__(self, test_X, test_y):
-        # self.data = pd.read_csv(os.getcwd() + "/data/diabetes.csv")
         self.labels = ["low", "medium", "high"]
         self.predicted = []
         self.probabilities = {0: {}, 1: {}}
         self.test_y = test_y
         self.test_X = test_X
 
+    # function to traverse through feautes and count the occurances of a nominal value
     def count(self, data, colname, label, target):
         condition = (data[colname] == label) & (data["Class"] == target)
         return len(data[condition])
 
     def fit(self, X):
-        # prior = train_df.groupby("class").size().div(len(train_df))
+        # count the probability of 1 or 0 in our train data
         count_0 = self.count(train_X, "Class", 0, 0)
         count_1 = self.count(train_X, "Class", 1, 1)
         prob_0 = count_0 / len(train_X)
         prob_1 = count_1 / len(train_X)
-        # print(count_0)
-        # print(count_1)
 
+        # traversing through all the features and calculate the probabilities of all unique values
+        # aka training the model
         for col in train_X.columns[:-1]:
             self.probabilities[0][col] = {}
             self.probabilities[1][col] = {}
@@ -35,10 +35,12 @@ class OwnNaiveBayes(BaseEstimator, ClassifierMixin):
 
                 self.probabilities[0][col][category] = count_ct_0 / count_0
                 self.probabilities[1][col][category] = count_ct_1 / count_1
-
+        # test the model
         for row in range(0, len(test_X)):
+            # final probabilites of which we will choose the highest probability
             prod_0 = prob_0
             prod_1 = prob_1
+            # P(row) = P(0)* P(feature) * P(feature) ...
             for feature in test_X.columns:
                 prod_0 *= self.probabilities[0][feature][test_X[feature].iloc[row]]
                 prod_1 *= self.probabilities[1][feature][test_X[feature].iloc[row]]
@@ -50,6 +52,9 @@ class OwnNaiveBayes(BaseEstimator, ClassifierMixin):
                 self.predicted.append(1)  #
 
     def predict(self, X):
+        # "confusion matrix"
+        # true positive, true negative, falsely positive, falsely negative
+        # compare the rtest set and our predicted results
         tp, tn, fp, fn = 0, 0, 0, 0
         for j in range(0, len(self.predicted)):
             if self.predicted[j] == 0:
@@ -63,7 +68,7 @@ class OwnNaiveBayes(BaseEstimator, ClassifierMixin):
                 else:
                     fn += 1
         return self.predicted
-
+        # accuracy = (tp+tn) / (p+n)
         # print(
         #     "Accuracy: ",
         #     ((tp + tn) / len(self.test_y)) * 100,
@@ -80,27 +85,16 @@ from sklearn.datasets import load_diabetes
 
 # %%
 if __name__ == "__main__":
-
-    # df = pd.read_csv(os.getcwd() + "/data/diabetes.csv")
-
-    # X = df[df.columns[:-1]]
-    # y = df["Class"]
-
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.7)
-    # clf = GaussianNB()
-    # clf.fit(X_train, y_train)
-    # print(
-    #     "Model accuracy score: {0:0.4f}".format(
-    #         accuracy_score(y_test, clf.predict(X_test))
-    #     )
-    # )
-
+    # temp = "/Users/tiimie/Desktop/tu/ml/ML-Crypto-Car-Project/EX2/data/diabetes.csv"
     data = pd.read_csv(os.getcwd() + "/data/diabetes.csv")
+    # data = pd.read_csv(temp)
     labels = ["low", "medium", "high"]
 
     for j in data.columns[:-1]:
         mean = data[j].mean()
+        # replacing the zero value with the mean value of the column
         data[j] = data[j].replace(0, mean)
+        # using pd's cut to convert numeric data to nominal storing them in bins that are labeled above
         data[j] = pd.cut(data[j], bins=len(labels), labels=labels)
 
     train_percent = 70
@@ -116,4 +110,3 @@ if __name__ == "__main__":
 
 
 # %%
-# Calculate min and standard deviation
