1e939d8a0a7ea531a39924b928d6b4c2a12a089c
beregmiklos <47614050+beregmiklos@users.noreply.github.com>
2023-03-01 13:50:55 +0100

    Update agent.py

--- EX3/agent.py
+++ EX3/agent.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 
 NUM_SPEEDS = 5
 NUM_ACTIONS = 9
@@ -17,7 +18,7 @@ class Agent:
 
 		self.acc_actions = [(1, 1), (0, 1), (1, 0), (0, 0), (-1, 0), (0, -1), (1, -1), (-1, 1), (-1, -1)]
 
-	def play_episode(self):
+	def play_episode(self, explore = True, learn = True):
 		
 		sequence = []
 		self.env.start()
@@ -26,21 +27,25 @@ class Agent:
 		while not self.env.done:
 			
 			state = self.env.state
-
-			# Get action based on state and either policy or random based on epsilon
-			action = self.explore(self.policy[state])
+			
+			if explore:
+				# Get action based on state and either policy or random based on epsilon
+				action = self.explore(self.policy[state])
+			else:
+				action = self.policy[state]
 
 			# Get step reward
 			reward, new_state, done = self.env.step(action)
 
 			# Add new state-action pair to sequence
 			sequence.append((state, action, reward))
-		
-		# Get returns for state-action pairs
-		returns = np.zeros(len(sequence))
-		for i in reversed(range(len(sequence))):
-			for j in range(i + 1):
-				returns[j] += sequence[i][2]
+
+		if learn:
+			# Get returns for state-action pairs
+			returns = np.zeros(len(sequence))
+			for i in reversed(range(len(sequence))):
+				for j in range(i + 1):
+					returns[j] += sequence[i][2]
 
 		# Get new action values for state-action pairs 
 		for seq, ret in zip(sequence, returns):
@@ -72,3 +77,19 @@ class Agent:
 					NUM_ACTIONS), dtype=np.int32)
 		self.policy = np.full((self.env.track.shape[1], self.env.track.shape[0], NUM_SPEEDS, NUM_SPEEDS), 3,
 							dtype=np.int32)
+	
+	def show_sequence(self, sequence, save_path=None, show_legend=True):
+		track = self.env.track.copy()
+
+		for item in sequence:
+			state = item[0]
+			track[state[1], state[0]] = 4
+
+		im = plt.imshow(track)
+
+		plt.axis("off")
+
+		if save_path is not None:
+			plt.savefig(save_path, bbox_inches="tight")
+
+		plt.show()
